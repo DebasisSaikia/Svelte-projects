35af63c1cdde2d2808cc24c8c5e601b53db1d3c6
Debasis Saikia <debasishsaikia136@gmail.com>
2021-05-29 19:00:41 +0530

    custom-component

--- polls-project/src/components/PollList.svelte
+++ polls-project/src/components/PollList.svelte
@@ -1,8 +1,20 @@
 <script>
+  import PollDetails from "./PollDetails.svelte";
+
   export let polls = [];
 </script>
 
-<div class="poll_list" />
+<div class="poll-list" />
+{#each polls as poll (poll.id)}
+  <div>
+    <PollDetails {poll} />
+  </div>
+{/each}
 
 <style>
+  .poll-list {
+    display: grid !important;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
 </style>
