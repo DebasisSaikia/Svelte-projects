f33bab38883dda33575419a391b75c584c46d614
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-07 13:47:00 +0530

    svelte-kit-project-setup

---
+++ svelte-kit-project/src/routes/about.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+    <title>Svelte Kit -About </title>
+</svelte:head>
+<h1>About Page</h1>
+<p>This is about page for our project</p>
---
+++ svelte-kit-project/src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+    console.log('svelte kit run')
+</script>
+<svelte:head>
+    <title>Svelte Kit -Homepage </title>
+</svelte:head>
+<h1>Svelte Kit Project</h1>
+<style>
+h1{
+    color: rgb(207, 207, 8);
+}
+</style>
