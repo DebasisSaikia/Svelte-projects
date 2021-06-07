e7f16a3f3cc5efba28f7b779a01a862cd3a169cb
Debasis Saikia <debasishsaikia136@gmail.com>
2021-06-07 17:22:58 +0530

    complete project

--- svelte-kit-project/src/components/pokeCard.svelte
+++ svelte-kit-project/src/components/pokeCard.svelte
@@ -4,7 +4,7 @@
 
 <a
 	class="p-8 bg-gray-200 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
-	href={`/pokeman/${pokeman.id}`}
+	href={`/pokemon/${pokeman.id}`}
 >
 	<img class="h-40" src={pokeman.image} alt={pokeman.name} />
 	<h2 class="uppercase text-bold">{pokeman.id} {pokeman.name}</h2>
---
+++ svelte-kit-project/src/routes/pokemon/[id].svelte
@@ -0,0 +1,15 @@
+<script context="module">
+	export async function load({ page }) {
+		const id = page.params.id;
+		const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+		const res = await fetch(url);
+		const pokeman = await res.json();
+		return { props: { pokeman } };
+	}
+</script>
+
+<script>
+	export let pokeman;
+</script>
+
+<h1 class="text-4xl text-center font-bold my-8 ">{pokeman.name}</h1>
