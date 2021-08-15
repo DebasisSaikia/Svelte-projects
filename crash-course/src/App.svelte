a657608cd6a72fdeb419c8a0c77cc63dc75bf1ff
Debasis Saikia <debasishsaikia136@gmail.com>
2021-08-15 15:20:34 +0530

    project initialise

---
+++ crash-course/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		height: 100vh;
+		border-radius: 5px;
+		margin: 0 auto;
+		background-color: rgb(10, 10, 10);
+	}
+
+	h1 {
+		/* color: #201f1f; */
+		text-transform: uppercase;
+		font-size: 10em;
+		font-weight: 500;
+		background: -webkit-linear-gradient(rgb(240, 135, 135), #333);
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
